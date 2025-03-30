6ceaa7ba8cf7af7ce45963853abd56e040df2097
Nandalala <56131112+NaNdalal-dev@users.noreply.github.com>
2025-03-30 15:36:39 +0530

    Update variables.py

---
+++ Python Basics/variables.py
@@ -0,0 +1,28 @@
+"""
+What is a Variable in Python?
+A variable in Python is a container for storing data values. 
+Unlike other programming languages, Python does not require explicit variable declaration.
+"""
+"""
+Example code below
+"""
+
+a = 1
+"""
+ variable 'a' is integer type 
+"""
+print(a)
+
+"""
+Variable 'name' is type string.
+Variable 'height' is type float.
+"""
+name = "Jai"
+height = 5.12
+
+print(name)
+print(height)
+
+"""
+Checkout Var-rule.txt to check rules for a variable.
+"""
