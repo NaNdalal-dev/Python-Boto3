1db2570a26d87e2f6dcd145320a0816f742f8e88
Nandalala <56131112+NaNdalal-dev@users.noreply.github.com>
2025-04-28 22:49:45 +0530

    Update Features.py

--- Python Basics/Data Types/Dictionary/Basics/Features.py
+++ Python Basics/Data Types/Dictionary/Basics/Features.py
@@ -2,12 +2,24 @@
 my_car = {
   "brand": "Ford",
   "model": "Mustang",
+  "year": 1964
+}
+
+#Ordered (>=3.7)
+print(my_car)
+
+#Mutable
+my_car["year"] = 2005
+my_car.pop("model")
+print(my_car)
+
+#No Duplicates
+my_car = {
+  "brand": "Ford",
+  "model": "Mustang",
   "year": 1964,
   "year":2005
 }
-
-
-#No Duplicates
+#Year Key repeated twice
 print(my_car)
-
 
